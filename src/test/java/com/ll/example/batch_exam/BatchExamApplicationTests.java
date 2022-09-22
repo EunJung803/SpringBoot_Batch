fd297f606c79b9ebd894fc4edafd82cb2f7776c2
EunJung803 <aimee3322@gmail.com>
2022-09-22 09:49:16 +0900

    Setting

---
+++ src/test/java/com/ll/example/batch_exam/BatchExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.example.batch_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
