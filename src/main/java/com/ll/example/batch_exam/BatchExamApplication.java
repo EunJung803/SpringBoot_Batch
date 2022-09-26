602d0c4f7b19d986b6cee900aea763dbe5321e87
EunJung803 <aimee3322@gmail.com>
2022-09-26 11:34:33 +0900

    Feat : DevInitData 샘플 회원 4명 생성

--- src/main/java/com/ll/example/batch_exam/BatchExamApplication.java
+++ src/main/java/com/ll/example/batch_exam/BatchExamApplication.java
@@ -3,9 +3,11 @@ package com.ll.example.batch_exam;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableBatchProcessing
+@EnableJpaAuditing
 public class BatchExamApplication {
 
     public static void main(String[] args) {
